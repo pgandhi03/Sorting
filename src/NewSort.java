670d8370150eb30c27fe3fe19a00f6c5ccb6cbc3
Prachi Gandhi <itsprachi03@gmail.com>
2019-03-25 16:41:33 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,15 @@
+
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BubbleSort ob = new BubbleSort();
+        int array[] = {52,45,98,36,47,332,6,26,14};
+        ob.bubbleSort(array);
+        System.out.println("Sorted");
+        ob.printArray(array);
+
+
+	}
+
+}
